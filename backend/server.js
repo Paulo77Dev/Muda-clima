538feadf19a6e5ebb1030267fadf7de64b784f1e
zAllaNz <allanmgoncalves00@gmail.com>
2025-02-21 22:22:37 -0300

    Configurando o banco de dados

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { Pool } = require('pg');
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mudaclima',
+    password: 'admin',
+    port: 5432,
+})
+
+app.get('/datasus', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM datasus LIMiT 10');
+        res.json(resultado.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro no servidor.')
+    }
+});
+
+app.listen(3000, () => console.log('API rodando na porta 3000'));
